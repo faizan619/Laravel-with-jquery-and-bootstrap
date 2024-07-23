ccf7511ee0ebc739e96bbca2de514379ddaacf21
faizan619 <alamf6023@gmail.com>
2024-07-24 00:47:41 +0530

    implement the bootstrap routes

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
 
-Route::get('/jquery',function(){
-    return view('jquery.home');
-})->name('Jhome');
+Route::get('/jquery',function(){return view('jquery.home');})->name('Jhome');
 
-Route::get('/bootstrap',function(){
-    return view('bootstrap.home');
-})->name('Bhome');
+Route::get('/bootstrap',function(){return view('bootstrap.home');})->name('Bhome');
+Route::get('/bootstrap/1',function(){return view('bootstrap.align_self');})->name('align_self');
+Route::get('/bootstrap/2',function(){return view('bootstrap.flex_direction');})->name('flex_direction');
+Route::get('/bootstrap/3',function(){return view('bootstrap.display');})->name('display');
