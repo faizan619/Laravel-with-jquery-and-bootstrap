9a1b1391fd1eae3b8a8d253678f385bc1ba96d33
Faizan Alam <alamf6023@gmail.com>
2024-07-23 16:25:44 +0530

    completed with margin and padding of bootstrap

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jquery',function(){
+    return view('jquery.home');
+})->name('Jhome');
+
+Route::get('/bootstrap',function(){
+    return view('bootstrap.home');
+})->name('Bhome');
